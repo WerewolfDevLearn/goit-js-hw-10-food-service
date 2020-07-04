64a81672562e526e20a99bdc35131c0873071850
Mikhail <werewolfdevlearn@gmail.com>
2020-07-04 20:46:38 +0300

    delete some logs

--- src/js/create-gallary.js
+++ src/js/create-gallary.js
@@ -3,6 +3,5 @@ import mealMenu from '../jsons/menu.json';
 
 const menuRef = document.querySelector('ul.js-menu');
 const markup = itemTamplate(mealMenu);
-console.log(markup);
 
 menuRef.insertAdjacentHTML('beforeend', markup);
